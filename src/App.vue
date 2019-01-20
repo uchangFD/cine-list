cf9a0598440dc50960b1956042609e0c9eddabcb
Dongwoo Lee <likedemian@me.com>
2019-01-20 20:45:39 +0900

    added vue router

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>여기서부터 코드를 시작합니다.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
