e2933c6ecef5ff7c11de837b1a974e7c342600b0
Dongwoo Lee <likedemian@me.com>
2019-02-21 16:54:07 +0900

    Added FontAwesome icon

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import store from './store'
 
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import {faAngleLeft, faThumbsUp, faBookmark, faEnvelope, faTimesCircle, faCheckCircle } from '@fortawesome/fontawesome-free-solid'
+import {faAngleLeft, faThumbsUp, faBookmark, faEnvelope, faTimesCircle, faCheckCircle, faTimes } from '@fortawesome/fontawesome-free-solid'
 // import { } from '@fortawesome/free-solid-svg-icons'
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -14,7 +14,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 // import { config } from './store/firebase.config'
 
 
-library.add(faAngleLeft, faThumbsUp, faBookmark, faEnvelope, faTimesCircle, faCheckCircle )
+library.add(faAngleLeft, faThumbsUp, faBookmark, faEnvelope, faTimesCircle, faCheckCircle, faTimes )
 Vue.component('FontAwesome', FontAwesomeIcon)
 
 
