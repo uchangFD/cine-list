6e28eb548f2f2df7ab5bb17a8236da7fc392f394
Dongwoo Lee <likedemian@me.com>
2019-02-27 17:00:16 +0900

    changed api variables name

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,13 @@
 import axios from 'axios'
 
 const API_KEY = '64391ca210dbae0d44b0a622177ef8d3'
-const LANGUAGE = 'en-US'
-const TRENDING = `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}&language=${LANGUAGE}&page=1`
-const UPCOMING = `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=${LANGUAGE}&page=1`
-const SEARCH = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=${LANGUAGE}&vote_count.gte=50&query=`
-const GENRES = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=${LANGUAGE}`
+const ROOT_URL = `https://api.themoviedb.org/3/`
+const LANGUAGE_KO = 'ko-KR'
+const LANGUAGE_EN = 'en-US'
+const TRENDING = `${ROOT_URL}trending/movie/week?api_key=${API_KEY}&language=${LANGUAGE_EN}&page=1`
+const UPCOMING = `${ROOT_URL}movie/upcoming?api_key=${API_KEY}&language=${LANGUAGE_EN}&page=1`
+const SEARCH = `${ROOT_URL}search/movie?api_key=${API_KEY}&language=${LANGUAGE_EN}&vote_count.gte=50&query=`
+const GENRES = `${ROOT_URL}genre/movie/list?api_key=${API_KEY}&language=${LANGUAGE_EN}`
 
 
 const request = (method, url, data) => {
@@ -19,21 +21,17 @@ export const mainSlide = {
   }
 }
 
-
 export const subSlide = {
   fetch() {
     return request('get', UPCOMING)
   }
 }
 
-
-
 export const search = {
   fetch(query) {
     return request('get', `${SEARCH + query}`)
   }
 }
-
 
 export const browser = {
   fetch(item) {
@@ -43,10 +41,9 @@ export const browser = {
 
 export const categories = {
   fetch(id, page) {
-    return request('get', `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=${LANGUAGE}&with_genres=${id}&vote_count.gte=100&page=${page}`)
+    return request('get', `${ROOT_URL}discover/movie?api_key=${API_KEY}&language=${LANGUAGE_EN}&with_genres=${id}&vote_count.gte=100&page=${page}`)
   }
 }
-
 
 export const genre = {
   fetch() {
@@ -56,33 +53,31 @@ export const genre = {
 
 export const content = {
   fetch(id) {
-    return request('get', `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=${LANGUAGE}`)
+    return request('get', `${ROOT_URL}movie/${id}?api_key=${API_KEY}&language=${LANGUAGE_EN}`)
   }
 }
 
-
-
 export const videos = {
   fetch(id) {
-    return request('get', `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}&language=${LANGUAGE}`)
+    return request('get', `${ROOT_URL}movie/${id}/videos?api_key=${API_KEY}&language=${LANGUAGE_EN}`)
   }
 }
 
 export const cast = {
   fetch(id) {
-    return request('get', `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${API_KEY}&language=${LANGUAGE}&append_to_response=movie_credits`)
+    return request('get', `${ROOT_URL}movie/${id}/credits?api_key=${API_KEY}&language=${LANGUAGE_EN}&append_to_response=movie_credits`)
   }
 }
 
 
 export const person = {
   fetch(id) {
-    return request('get', `https://api.themoviedb.org/3/person/${id}?api_key=${API_KEY}&language=${LANGUAGE}`)
+    return request('get', `${ROOT_URL}person/${id}?api_key=${API_KEY}&language=${LANGUAGE_EN}`)
   }
 }
 
 export const personCredits = {
   fetch(id) {
-    return request('get', `https://api.themoviedb.org/3/person/${id}/movie_credits?api_key=${API_KEY}&language=${LANGUAGE}`)
+    return request('get', `${ROOT_URL}person/${id}/movie_credits?api_key=${API_KEY}&language=${LANGUAGE_EN}`)
   }
 }
